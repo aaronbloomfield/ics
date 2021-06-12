8d6bc052c05ae7777f9a97ba689e704376a295c7
Aaron Bloomfield <aaron@virginia.edu>
2021-06-12 10:45:10 -0400

    Quick update to python cryptomoney.sh file

--- hws/cryptocurrency/python/cryptomoney.sh
+++ hws/cryptocurrency/python/cryptomoney.sh
@@ -8,43 +8,43 @@ case "$1" in
 	  # additional parameters provided: (none)
 	  ;;
 
-    genesis) python cmoney.py genesis block_0.txt
+    genesis) python3 cmoney.py genesis block_0.txt
 	     # additional parameters provided: (none)
              ;;
 
-    generate) python cmoney.py generate $2
+    generate) python3 cmoney.py generate $2
 	      # additional parameters provided: the wallet file name
               ;;
 
-    address) python cmoney.py address $2
+    address) python3 cmoney.py address $2
 	     # additional parameters provided: the file name of the wallet
 	     ;;
 
-    fund) python cmoney.py fund $2 $3 $4
+    fund) python3 cmoney.py fund $2 $3 $4
 	  # additional parameters provided: destination wallet
 	  # address, the amount, and the transaction file name
           ;;
 
-    transfer) python cmoney.py transfer $2 $3 $4 $5
+    transfer) python3 cmoney.py transfer $2 $3 $4 $5
 	      # additional parameters provided: source wallet file
 	      # name, destination address, amount, and the transaction
 	      # file name
 	      ;;
 
-    balance) python cmoney.py balance $2
+    balance) python3 cmoney.py balance $2
 	     # additional parameters provided: wallet address
 	     ;;
 
-    verify) python cmoney.py verify $2 $3
+    verify) python3 cmoney.py verify $2 $3
 	    # additional parameters provided: wallet file name,
 	    # transaction file name
 	    ;;
 
-    mine) python cmoney.py mine $2
+    mine) python3 cmoney.py mine $2
 		 # additional parameters provided: difficulty
 		 ;;
     
-    validate) python cmoney.py validate
+    validate) python3 cmoney.py validate
 	      # additional parameters provided: (none)
 	      ;;
 
