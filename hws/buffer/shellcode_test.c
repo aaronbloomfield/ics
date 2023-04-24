97bffec712233ae3f78a4ca96c1b29d1222788b3
Aaron Bloomfield <aaron@virginia.edu>
2023-04-23 21:45:04 -0400

    buffer hw updates

--- hws/buffer/shellcode_test.c
+++ hws/buffer/shellcode_test.c
@@ -7,6 +7,7 @@ void main() {
   int on_stack;
   mprotect((char *)((long)&on_stack & -0x1000), 1, PROT_READ | PROT_WRITE | PROT_EXEC);
   mprotect((char *)((long)&main & -0x1000), 1, PROT_READ | PROT_WRITE | PROT_EXEC);
+  printf ("Before shell code is executed.\n");
   shellcode();
   printf ("After shell code is executed.\n");
 }
